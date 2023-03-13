fd5586788c634d6a4a0bc40456a6184e520e27e3
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-03-13 14:24:33 -0600

    More updates

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello world');
+// Code inspired by this article: https://www.geeksforgeeks.org/node-js-fs-rename-method/
+
+// Import filesystem module
+const fs = require('fs');
+const directoryWithFilesToRename = 'files-to-rename';
+
+// List all the filenames before renaming
+getCurrentFilenames();
+
+// Rename the file
+fs.rename(
+  `${directoryWithFilesToRename}/hello.txt`,
+  `${directoryWithFilesToRename}/world.txt`,
+  () => {
+    console.log('\nFile Renamed!\n');
+
+    // List all the filenames after renaming
+    getCurrentFilenames();
+  }
+);
+
+// Function to get current filenames
+// in directory
+function getCurrentFilenames() {
+  console.log('Current filenames:');
+  fs.readdirSync(directoryWithFilesToRename).forEach((file) => {
+    console.log(file);
+  });
+}
